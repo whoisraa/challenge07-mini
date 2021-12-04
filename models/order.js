3b5998f3b0ae9e07f8095c999381f1a6ddf2d0a3
whoisraa <raainvictus@gmail.com>
2021-12-04 23:45:15 +0700

    updated order model

--- models/order.js
+++ models/order.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Order.belongsTo(models.Product, {foreignKey: 'product_id'});
+      // Order.belongsTo(models.User, {foreignKey: 'user_id'});
     }
   };
   Order.init({
